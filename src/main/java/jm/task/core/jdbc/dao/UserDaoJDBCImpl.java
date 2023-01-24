484f1777ea96d7220d3ed86b8131c0696cee5574
Vladimir <yalt.vladimir@gmail.com>
2023-01-24 19:40:35 +0300

    Test are ok

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc.dao;
 
+import antlr.CppCodeGenerator;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
@@ -13,172 +14,78 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-        Connection connection = null;
-        try {
-            connection = Util.createConnection();
-            if (!tableExistsSQL(connection, "Users")) {
-                StringBuilder queryCreate = new StringBuilder("create table Users");
-                queryCreate.append(" ( id int auto_increment not null, ");
-                queryCreate.append(" name varchar(20), ");
-                queryCreate.append(" lastName VARCHAR(25), ");
-                queryCreate.append(" age int, ");
-                queryCreate.append(" PRIMARY KEY ( id ))");
-                Statement statement = connection.createStatement();
-                statement.execute(queryCreate.toString());
-            } else {
-                System.out.println("The Table Users already exists");
-            }
+        try (Connection connection = Util.createConnection()) {
+            StringBuilder queryCreate = new StringBuilder("create table if not exists Users");
+            queryCreate.append(" ( id int auto_increment not null, ");
+            queryCreate.append(" name varchar(20), ");
+            queryCreate.append(" lastName VARCHAR(25), ");
+            queryCreate.append(" age int, ");
+            queryCreate.append(" PRIMARY KEY ( id ))");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(queryCreate.toString());
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
-
     }
 
     public void dropUsersTable() {
-
-        Connection connection = null;
-        try {
-            connection = Util.createConnection();
-            if (tableExistsSQL(connection, "Users")) {
-                String queryDelete = "Drop table Users";
+        try(Connection connection = Util.createConnection()) {
+                String queryDelete = "Drop table if exists Users";
                 Statement statement = connection.createStatement();
-                statement.execute(queryDelete);
-            } else {
-                System.out.println("The table is not available");
-            }
-        } catch (SQLException e) {
+                statement.executeUpdate(queryDelete);
+            } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
-        Connection connection = null;
-        try {
-            connection = Util.createConnection();
-            if (tableExistsSQL(connection, "USERS")) {
-                String queryAdd = "insert into Users (name, lastName, age) "
-                        + "values (\"" + name + "\"," + " \"" + lastName + "\"," + age + ")";
-                Statement statement = connection.createStatement();
-                statement.executeUpdate(queryAdd);
-            } else {
-                System.out.println("The table is not available");
-            }
+        try (Connection connection = Util.createConnection()) {
+            String queryAdd = "INSERT INTO Users (name, lastName, age) VALUES (?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(queryAdd);
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 
     public void removeUserById(long id) {
-        Connection connection = null;
-        try {
-            connection = Util.createConnection();
-            if (!tableExistsSQL(connection, "Users")) {
-                String queryDelete = "DELETE FROM Users WHERE id = " + id + ";";
-                Statement statement = connection.createStatement();
-                statement.executeUpdate(queryDelete);
-            } else {
-                System.out.println("The table is not available");
-            }
+        try (Connection connection = Util.createConnection()) {
+            String queryDelete = "DELETE FROM Users WHERE id = " + id + ";";
+            PreparedStatement statement = connection.prepareStatement(queryDelete);
+            statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
-
     }
 
     public List<User> getAllUsers() {
-        Connection connection = null;
-        try {
-            connection = Util.createConnection();
-            if (tableExistsSQL(connection, "Users")) {
-                String queryAll = "select * from Users";
-                List<User> users = new ArrayList<>();
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(queryAll);
-                while (resultSet.next()) {
-                    String name = resultSet.getString("name");
-                    String lastname = resultSet.getString("lastName");
-                    byte age = resultSet.getByte("age");
-                    users.add(new User(name, lastname, age));
-                }
-                return users;
-            } else {
-                System.out.println("The table is not available allUsers");
+        try (Connection connection = Util.createConnection()) {
+            String queryAll = "select * from Users";
+            List<User> users = new ArrayList<>();
+            PreparedStatement statement = connection.prepareStatement(queryAll);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String lastname = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                users.add(new User(name, lastname, age));
             }
+            return users;
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
-        return null;
     }
 
     public void cleanUsersTable() {
-        Connection connection = null;
-
-        try {
-            connection = Util.createConnection();
-            if (tableExistsSQL(connection, "Users")) {
+        try(Connection connection = Util.createConnection()) {
                 String queryClean = "TRUNCATE TABLE Users";
                 Statement statement = connection.createStatement();
                 statement.execute(queryClean);
-            } else {
-                System.out.println("The table is not available");
-            }
-        } catch (SQLException e) {
+            } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
-    }
-
-    boolean tableExistsSQL(Connection connection, String tableName) throws SQLException {
-        DatabaseMetaData meta = connection.getMetaData();
-        ResultSet resultSet = meta.getTables(null, null, tableName, new String[]{"TABLE"});
-        return resultSet.next();
     }
 }
